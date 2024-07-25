d52d22565cecbf3141ff65a2c05adcb608f771b8
MikaWishaupt49 <99985506+MikaWishaupt49@users.noreply.github.com>
2024-07-25 11:38:22 +0200

    Update Website
    Major style overhaul with animated background

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var video = document.getElementById("myVideo");
+    video.playbackRate = 0.5; // Change this value to adjust the speed
+});
